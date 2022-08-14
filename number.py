b1970576a645871ed65e00d9e399a6af2ba47d4a
CHOE BYEONGSEON <qudtjs3012@naver.com>
2022-08-14 13:15:56 +0900

    number

---
+++ number.py
@@ -0,0 +1,31 @@
+#숫자형(数字のタイプ)
+
+#정수형,整数形
+a = 123
+b = - 178
+c = 0
+
+print(a, b, c)
+
+#실수형,実数形
+d = 1.2
+e = -3.53
+
+f = 4.24E10
+g = 4.24e-10
+
+print(d, e, f ,g)
+
+#사칙연산,四則演算
+
+h = 3
+i = 4
+print(h + i) #足し算
+print(h * i) #掛け算
+print(h / i) #割り算
+print(h - i) #引き算
+print(h ** i)
+print(h % i) #あまり
+print(i % h)
+print(i / h)
+print(i // h) #商
