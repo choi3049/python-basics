9537eae3af98818e464f35a01d9db0eb4d45a3ed
CHOE BYEONGSEON <qudtjs3012@naver.com>
2022-08-16 10:11:49 +0900

    input-output

---
+++ input-output.py
@@ -0,0 +1,14 @@
+#input
+
+'''
+number = input("숫자를 입력하세요: ")
+print(number)
+'''
+
+print("life" "is" "too short") # ①
+print("life"+"is"+"too short") # ②
+
+print("life", "is", "too short")
+
+for i in range(10):
+    print(i, end=' ')
