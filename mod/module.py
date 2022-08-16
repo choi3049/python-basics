bbb70695eefe6a7b0254b9d5a31b7175e5fae88f
CHOE BYEONGSEON <qudtjs3012@naver.com>
2022-08-16 14:02:54 +0900

    module

---
+++ mod/module.py
@@ -0,0 +1,13 @@
+# from mod1 import add
+# from mod1 import add, sub
+from mod1 import *
+import mod2
+
+print(add(3,4))
+
+print(mod2.PI)
+
+a = mod2.Math()
+print(a.solv(2))
+
+print(mod2.add(mod2.PI, 4.4))
